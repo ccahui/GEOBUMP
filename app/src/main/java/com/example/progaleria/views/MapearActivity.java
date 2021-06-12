bcf562950c1e9b9ded272502c159559461c0b8a9
raul96960 <raul96.960@gmail.com>
2021-06-11 20:12:11 -0500

    creando MapearActivity

---
+++ app/src/main/java/com/example/progaleria/views/MapearActivity.java
@@ -0,0 +1,15 @@
+package com.example.progaleria.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.progaleria.R;
+
+public class MapearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapear);
+    }
+}
